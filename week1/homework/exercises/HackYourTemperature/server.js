f48d5e067cd48f6bb4d90410eed1fa571dc18b2d
Burak <isikburak142@gmail.com>
2021-04-19 19:12:48 +0200

    Hackyourfuture homework of week2 is done

--- week1/homework/exercises/HackYourTemperature/server.js
+++ week1/homework/exercises/HackYourTemperature/server.js
@@ -1,6 +1,23 @@
-const express = require('express');
-const server  = express();
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const server = express();
+
+server.use(express.json());
+
+server.set("view engine", "handlebars");
+server.engine("handlebars", expressHandlebars({ defaultLayout: false }));
+
 server.get(`/`, (req, res) => {
-  res.send(`hello from backend to frontend!`);
+  res.render(`index`);
 });
+
+//
+server.post(`/weather`, (req, res) => {
+  const { cityName } = req.body;
+  res.send(cityName);
+});
+
 server.listen(3000);
+
+// https://github.com/ericf/express-handlebars/tree/master/examples/basic
+// the link that i benefit from
